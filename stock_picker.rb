5418e24d220578a81d26b5bfe2ef1be1be8005c3
jsun3886 <jsunday86@gmail.com>
2022-11-20 22:23:07 -0500

    update the stock_picker.rb to find optimum buy sell values
    Will search through the prices array and compares the current
    best buy sell price with the difference between the current price
    and the max of the rest of the array. If the new value is larger
    updates the buy and sell position.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,12 +1,22 @@
 def stock_picker(prices)
-    buy_sell=prices[-1]-prices[0]
-    puts buy_sell
+    buy =0
+    sell = prices.length-1
+    max=prices[sell]-prices[buy]
+    prices.each_with_index do |price,i|
+        if max < prices[i..].max-price
+            max = prices[i..].max-price
+            buy=i
+            sell = prices.rindex(prices[i..].max)
+        end
 
-    return [0,prices.length]
+    end
+
+    return [buy,sell]
 
 end
 
 
+prices =[17,3,6,9,15,8,6,1,10]
+x=7
 
-
-puts stock_picker([17,3,6,9,15,8,6,1,10])
+puts stock_picker(prices)
